31b1daea651a8730c2787036dc504819efe81ac2
cakejune <tyq444@gmail.com>
2023-06-01 12:15:43 -0400

    edited intro of infoModal

--- src/Navbar Elements/InfoModal.js
+++ src/Navbar Elements/InfoModal.js
@@ -3,7 +3,7 @@ import Modal from "react-bootstrap/Modal";
 
 export default function InfoModal({show, handleClose}){
     return(
-        <Modal show={show} onHide={handleClose} style={{fontFamily: "Roboto Mono, monospace"}}>
+      <Modal show={show} onHide={handleClose} style={{fontFamily: "Roboto Mono, monospace"}}>
       <Modal.Header closeButton>
         <Modal.Title className="welcomeMessage">
           <div className="welcomeText">Welcome to Iridiom</div>
@@ -11,7 +11,37 @@ export default function InfoModal({show, handleClose}){
       </Modal.Header>
       <Modal.Body>
         <div className="modalBodyContent">
-          <div className="Meaning">Here you'll find a periodic table of Idioms.</div>
+          <div className="Meaning">
+            <p>
+              At Iridiom, we turn learning English idioms into an elemental experience, similar to exploring the periodic table. 
+              Iridiom, a playful twist on the element 'Iridium' and 'idiom', serves as your portal to an interactive and vibrant world of English idioms. 
+            </p>
+            <p>
+              Here's a quick guide on how to use Iridiom:
+            </p>
+            <ol>
+              <li>
+                <strong>Exploring Idioms: </strong>
+                Each cell on our 'Periodic Table of Idioms' represents a unique idiom, labeled with a distinctive abbreviation. 
+                The cells are color-coded based on their categories for easy navigation. 
+                Click on any cell to uncover the phrase, meaning, usage, and associated tags of each idiom.
+              </li>
+              <li>
+                <strong>Search Function: </strong>
+                Looking for something specific? Use the search bar at the top of the page. 
+                As you type, non-matching idioms will fade, leaving only the idioms containing your search phrase. 
+                It's a dynamic way to discover idioms related to specific themes or concepts.
+              </li>
+              <li>
+                <strong>Category Filter: </strong>
+                Intrigued by a particular category? Use the category key below the table. 
+                Clicking on a category will dim idioms from other categories, allowing you to focus on idioms from your chosen category.
+              </li>
+            </ol>
+            <p>
+              We invite you to embark on this educational exploration, dive into the world of idioms, and most importantly, have fun! Welcome to Iridiom!
+            </p>
+          </div>
         </div>
       </Modal.Body>
       <Modal.Footer>
@@ -20,5 +50,6 @@ export default function InfoModal({show, handleClose}){
         </Button>
       </Modal.Footer>
     </Modal>
+    
     )
 }
